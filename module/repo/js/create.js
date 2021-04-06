3533cd18f3146f24e922eaa0b7ef0f11c0e3aa5d
gxy <guanxiying@easycorp.ltd>
2021-04-06 11:03:59 +0800

    * Add gitlab toggle codes.

--- module/repo/js/create.js
+++ module/repo/js/create.js
@@ -6,20 +6,39 @@ $(function()
         $form = $(this).closest('form');
         $form.css('min-height', $form.height());
     })
+    
+    $('#gitlabHost, #gitlabToken').change(function()
+    {
+        host  = Base64.encode($('#gitlabHost').val());
+        token = $('#gitlabToken').val();
+        url   = createLink('repo', 'ajaxgetgitlabprojects', "host=" + host + '&token=' + token);
+        if(host == '' || token == '') return false;
+
+        $.get(url, function(response)
+        {
+            $('#gitlabProject').append(response);
+            $('#gitlabProject').chosen().trigger("chosen:updated");;
+        });
+    });
 });
 
-function scmChanged(scm) {
+function scmChanged(scm)
+{
     if(scm == 'Git')
     {
         $('.account-fields').addClass('hidden');
 
         $('.tips-git').removeClass('hidden');
         $('.tips-svn').addClass('hidden');
-    } else
+    } 
+    else
     {
         $('.account-fields').removeClass('hidden');
 
         $('.tips-git').addClass('hidden');
         $('.tips-svn').removeClass('hidden');
     }
+
+    $('tr.gitlab').toggle(scm == 'Gitlab');
+    $('tr.hide-gitlab').toggle(scm != 'Gitlab');
 }
